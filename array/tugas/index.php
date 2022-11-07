ee27f5994f9ceb43f2ca924ffd8cd50fa234a946
NazwaChairunnisa <nazwachairunnisa1706@gmail.com>
2022-11-07 13:48:39 +0700

    belajar web

---
+++ array/tugas/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//Tugas Array
+$bestie1 =['aida','sukma','nikita','aliya','tiara'];
+$bestie2 =['nitta','awa','ike','caca','indira'];
+
+echo $bestie1[0]," ",$bestie2[0]," ",$bestie1[1]," ",$bestie2[1]," ",$bestie1[2]," ",$bestie2[2]," ",$bestie1[3]," ",$bestie2[3]," ",$bestie1[4]," ",$bestie2[04];
+echo "<br />";
+
+//tambah bestie
+array_push($bestie1,'firdis');
+array_push($bestie2,'sila');
+
+echo $bestie1[0]," ",$bestie2[0]," ",$bestie1[1]," ",$bestie2[1]," ",$bestie1[2]," ",$bestie2[2]," ",$bestie1[3]," ",$bestie2[3]," ",$bestie1[4]," ",$bestie2[04]," ",$bestie1[5]," ",$bestie2[5];
+
+?> 
