bcc4d5636fccc6bda83824092506e19b76558bc9
NazwaChairunnisa <nazwachairunnisa1706@gmail.com>
2022-12-05 11:06:54 +0700

    akhir

---
+++ tugas/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//Tugas Array
+$bestie1 =['Aini','Gresia','Nayla','Nazwa','Maya'];
+$bestie2 =['Nitta','Awa','Yurike','Caca','Sasa'];
+
+echo $bestie1[0]," ",$bestie2[0]," ",$bestie1[1]," ",$bestie2[1]," ",$bestie1[2]," ",$bestie2[2]," ",$bestie1[3]," ",$bestie2[3]," ",$bestie1[4]," ",$bestie2[04];
+echo "<br />";
+
+//tambah bestie
+array_push($bestie1,'Maurel');
+array_push($bestie2,'Disty');
+
+echo $bestie1[0]," ",$bestie2[0]," ",$bestie1[1]," ",$bestie2[1]," ",$bestie1[2]," ",$bestie2[2]," ",$bestie1[3]," ",$bestie2[3]," ",$bestie1[4]," ",$bestie2[04]," ",$bestie1[5]," ",$bestie2[5];
+
+?>  
